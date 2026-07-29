f14c25c684416f7d2c9e2de0d11e0d124c7c5d1a
Alex MJ <alexmjvkd@gmail.com>
2026-07-29 10:41:00 +0530

    Update maximum-product-of-three-numbers.cpp

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums)
     {
-        // Step 1: Sort the array
+        
         sort(nums.begin(), nums.end());
 
-        // Step 2: Store the size of the array
         int n = nums.size();
 
-        // Step 3: Product of the three largest numbers
         int product1 = nums[n-1] * nums[n-2] * nums[n-3];
 
-        // Step 4: Product of the two smallest and the largest
         int product2 = nums[0] * nums[1] * nums[n-1];
 
-        // Step 5: Return the larger product
         return max(product1, product2);
     }
 };
