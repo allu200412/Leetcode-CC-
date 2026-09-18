a20180726871502e8f62dc5527ddf046b70cafd1
Alex MJ <alexmjvkd@gmail.com>
2026-09-18 11:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (94.30%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+
+        preorder(root, result);
+
+        return result;
+    }
+
+private:
+    void preorder(TreeNode* node, vector<int>& result) {
+        if (node == nullptr) {
+            return;
+        }
+
+        result.push_back(node->val);
+
+        preorder(node->left, result);
+        preorder(node->right, result);
+    }
+};
